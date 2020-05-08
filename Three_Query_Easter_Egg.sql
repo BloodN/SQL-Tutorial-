fd4dd33ccbd54b4d9699938582080c57f7166d82
BloodN <60768545+BloodN@users.noreply.github.com>
2020-05-07 21:15:08 -0400

    Add files
    Add files

---
+++ Three_Query_Easter_Egg.sql
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2512
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red223\green223\blue223;\red0\green0\blue0;
+\red98\green0\blue117;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c89804\c89804\c89804;\cssrgb\c0\c0\c0;
+\cssrgb\c46667\c0\c53333;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 SELECT * FROM 	 MOCK_DATA\
+\pard\pardeftab720\partightenfactor0
+{\field{\*\fldinst{HYPERLINK "http://localhost:8888/phpMyAdmin/url.php?url=https://dev.mysql.com/doc/refman/5.5/en/select.html"}}{\fldrslt 
+\f1\fs21\fsmilli10660 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 SELECT}}
+\f1\fs21\fsmilli10660 \cf4 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \'a0Hello\'a0\cf5 \strokec5 FROM\cf4 \strokec4 \'a0MOCK_DATA\
+INSERT INTO `Python` (`Print`, `Hello`, `World`) VALUES ('What did the Java code say to the C code? You\'92ve got no class', 'Why did the database administrator leave his wife? She had one-to-many relationships', 'What did the SQL query ask the tables in the bar? May I JOIN you?\'92);\'cd}
